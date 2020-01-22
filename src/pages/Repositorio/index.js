3eee2a30b5d2d836676008e7ee7fde94e398f2be
Wilmar Oliveira <wilmar_oliveira13@hotmail.com>
2020-01-21 21:30:04 -0300

    Implementando exibição de detalhes dos repositórios

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api';
+import { Container } from './styles';
 
-function Repositorio() {
+function Repositorio({match}) {
+
+  const [repositorios, setRepositorios] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+
+    async function load() {
+      const nomeRepo = decodeURIComponent(match.params.repositorio);
+
+      const [repositorioData, issuesData] = await Promise.all([
+        api.get(`/repos/${nomeRepo}`),
+        api.get(`/repos/${nomeRepo}/issues` , {
+          params: {
+            state: 'open',
+            per_page: 5
+          }
+        })
+      ]);
+
+      setRepositorios(repositorioData.data);
+      setIssues(issuesData.data);
+      setLoading(false);
+
+      
+    }
+
+    load();
+    
+  },[match.params.repositorio]);
+
+
   return(
-    <div>
-      <h1>Página Repositório</h1>
-    </div>
+    <Container>
+
+    </Container>
   );
 }
 
