e332b8904e2381ae56dc896fc80ba0942dcdded3
Wilmar Oliveira <wilmar_oliveira13@hotmail.com>
2020-01-23 21:47:56 -0300

    Construindo página de exibição de detalhes

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import api from '../../services/api';
-import { Container } from './styles';
+import { Container, Owner, Loading, BackButton } from './styles';
+import { FaArrowLeft } from 'react-icons/fa'
 
 function Repositorio({match}) {
 
@@ -34,10 +35,27 @@ function Repositorio({match}) {
     
   },[match.params.repositorio]);
 
+  if(loading) {
+    return(
+      <Loading>
+        <h1>Carregando...</h1>
+      </Loading>
+    );
+    
+  }
+
 
   return(
     <Container>
+      <BackButton to="/" >
+        <FaArrowLeft size={20} />
+      </BackButton>
 
+      <Owner>
+        <img src={repositorios.owner.avatar_url} alt={repositorios.owner.login} />
+        <h1>{repositorios.name}</h1>
+        <p>{repositorios.description}</p>
+      </Owner>
     </Container>
   );
 }
--- src/pages/Repositorio/styles.js
+++ src/pages/Repositorio/styles.js
@@ -1,5 +1,51 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const Container = styled.div`
-  
+  max-width: 700px;
+  padding: 30px;
+  margin: 80px auto;
+  background: #fff;
+  border-radius: 8px;
+`;
+
+export const Owner = styled.header`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    img {
+        max-width: 130px;
+        margin: 20px 0 20px;
+    }
+
+    h1 {
+        color: #0D2636;
+        text-transform: uppercase;
+    }
+
+    p {
+        color: #000;
+        line-height: 3.4;
+        font-size: 14px;
+    }
+`;
+
+export const Loading = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    height: 100vh;
+`;
+
+export const BackButton = styled(Link)`
+    color: #000;
+    border: 0;
+    outline: 0;
+    background: transparent;
+
+    :hover {
+        color: #0D2636;
+    }
 `;
